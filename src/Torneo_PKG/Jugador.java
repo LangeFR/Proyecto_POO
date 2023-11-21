a2be2585dbae9abd2a6377e06a4c58c5221d70d7
Mateo Vanegas <mdjvanegasc@gmail.com>
2023-11-21 12:20:18 -0500

    Declare y comente metodos de Jugador

--- src/Torneo_PKG/Jugador.java
+++ src/Torneo_PKG/Jugador.java
@@ -15,13 +15,18 @@ public class Jugador {
         this.nickName = nickName;
         partidas = new ArrayList<>();
     }
-    public void agregarPartida(Partida partida){partidas.add(partida);}
+
+    /*
+     * Getters
+     */
     public String getNombre() {return nombre;}
     public String getCorreo() {return correo;}
     public String getNickName() {return String.valueOf(nickName);}
     public Partida getPartida(int i){return partidas.get(i);}
 
-    
+    /*
+     * Setters
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     } public void setCorreo(String correo) {
@@ -31,12 +36,101 @@ public class Jugador {
     } public void setPartida(int i, Partida partida){
         partidas.set(i, partida);
     }
-    
-    public int PuntajeAcumulado(){
+
+    /*
+     * Recibe una partida y la agrega a la lista
+     * No retorna nada
+     */
+    public void agregarPartida(Partida partida){partidas.add(partida);}
+
+    /*
+     * Retorna la suma del puntaje de todas las partidas del jugador     
+    */    
+    public int puntajeAcumulado(){
         int puntaje = 0;
         for (int i = 0; i < partidas.size(); i++) {
             puntaje += partidas.get(i).getPuntaje();
         }
         return puntaje;
     }
+
+    /*
+     * Recibe un juego y suma el total de puntos
+     * Retorna la suma de puntos
+     * -----------------------------------------------------------------
+     */
+    public int puntajeAcumulado(Juego juego){
+        //Por implementar
+        return -1;
+    }
+
+    /*
+     * Recibe un juego y retorna el mayor puntaje
+     * -----------------------------------------------------------------
+     */
+    public int mejorPuntaje(Juego juego){
+        //Por implementar
+        return -1;
+    }
+
+    /*
+     * Recibe un juego e identifica el mayor puntaje
+     * Retorna la partida con dicho puntaje
+     * -----------------------------------------------------------------
+     */
+    public Partida mejorPartida(Juego juego){
+        //Por implementar
+        return null;
+    }
+
+    /*
+     * No recibe nada.
+     * Retorna el juego con mayor puntaje 
+     * -----------------------------------------------------------------
+     */
+    public Juego mejorJuego(){
+        //por implementar
+        return null;
+    }
+
+    /*
+     * No recibe nada. 
+     * Retorna la cantidad de partidas totales (en los 4 juegos) del jugador
+     * -----------------------------------------------------------------
+     */
+    public int cantidadPartidas(){
+        //por implementar
+        return -1;
+    }
+
+    /*
+     * Recibe un juego
+     * Retorna la cantidad de partidas jugadas de ese juego
+     * -----------------------------------------------------------------
+     */
+    public int cantidadPartidas(Juego juego){
+        //por implementar
+        return -1;
+    }
+
+    /*
+     * No recibe nada
+     * Retorna la cantidad de horas jugadas totales (en los 4 juegos) del jugador
+     * -----------------------------------------------------------------
+     */
+    public int horasJugadas(){
+        //por implementar;
+        return -1;
+    }
+
+    /*
+     * Recibe un juego
+     * Retorna la cantidad de horas jugadas de ese juego
+     * -----------------------------------------------------------------
+     */
+    public int horasJugadas(Juego juego){
+        //por implementar
+        return -1;
+    }
+
 }
