85153e79271c8d16ae63fee8b06de3a849edc5fd
Mateo Vanegas <mdjvanegasc@gmail.com>
2023-11-24 21:48:25 -0500

    :bug: Cambie nombre clase

--- src/Torneo_PKG/Fortnite.java
+++ src/Torneo_PKG/Fortnite.java
@@ -1,13 +1,13 @@
 package Torneo_PKG;
 
-public class Fortine extends Juego{
+public class Fortnite extends Juego{
     int kills;
 
-    public Fortine(String nombre, String casaMatriz, int cantjugadores, String tipoJuego, int kills) {
+    public Fortnite(String nombre, String casaMatriz, int cantjugadores, String tipoJuego, int kills) {
         super(nombre, casaMatriz, cantjugadores, tipoJuego);
         this.kills = kills;
     }
-    public Fortine(String nombre, String casaMatriz, int cantjugadores, String tipoJuego) {
+    public Fortnite(String nombre, String casaMatriz, int cantjugadores, String tipoJuego) {
         this(nombre, casaMatriz, cantjugadores, tipoJuego, 0);
     }
 
