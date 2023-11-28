d5a780a92672d3c7a01c5173deeb563973dda952
Mateo Vanegas <mdjvanegasc@gmail.com>
2023-11-28 00:02:22 -0500

    :construction: Preparacion para Competencia.bin

--- src/Testing/Competencia_Test.java
+++ src/Testing/Competencia_Test.java
@@ -15,23 +15,38 @@ public class Competencia_Test {
         ApexLegends apexLegends = new ApexLegends("ApexLegends", "Respawn Entertainment", 50, "Numero de kills");
         Tetris tetris = new Tetris("Tetris", "Alekséi Pázhitnov", 1, "Acumulacion de puntos");
             
-        Jugador jugador = new Jugador("Pepe", "pepe123@gmail.com", "pepito");
+        //Definir jugadores
+        Jugador jugador = new Jugador("Juan", "Juan@gmail.com", "Jaunito");
+        Jugador jugador = new Jugador("Pepe", "pepe@gmail.com", "Pepegrillo");
+        Jugador jugador = new Jugador("Alice", "Alice@hotmail.com", "Aliceee");
+        Jugador jugador = new Jugador("Bob", "Bob@gmail.com", "Bob");
 
-        Partida partida1 = new Partida("2023_11_26", 3600, 36, fortnite);
-        Partida partida2 = new Partida("2023_1_6", 2000, 40, fortnite);
-        Partida partida3 = new Partida("2023_12_2", 3600, 10, ajedrez);
-        Partida partida4 = new Partida("2023_6_16", 700, 80, tetris);
-        Partida partida5 = new Partida("2023_3_4", 1600, 22, fortnite);
-        Partida partida6 = new Partida("2023_24_2", 1000, 18, apexLegends);
-        Partida partida7 = new Partida("2023_1_7", 4600, 0, ajedrez);
 
-        jugador.agregarPartida(partida1);
-        jugador.agregarPartida(partida2);
-        jugador.agregarPartida(partida3);
-        jugador.agregarPartida(partida4);
-        jugador.agregarPartida(partida5);
-        jugador.agregarPartida(partida6);
-        jugador.agregarPartida(partida7);
+        //Partidas Juan
+        Partida partidaJ1 = new Partida("2023_11_26", 3600, 36, fortnite);
+        Partida partidaJ2 = new Partida("2023_1_6", 2000, 40, fortnite);
+        Partida partidaJ3 = new Partida("2023_12_2", 3600, 10, ajedrez);
+        Partida partidaJ4 = new Partida("2023_6_16", 700, 80, tetris);
+        Partida partidaJ5 = new Partida("2023_3_4", 1600, 22, fortnite);
+        Partida partidaJ6 = new Partida("2023_24_2", 1000, 18, apexLegends);
+        Partida partidaJ7 = new Partida("2023_1_7", 4600, 0, ajedrez);
+
+        //Partidas Pepe
+        Partida partidaP1 = new Partida("2023_11_26", 3600, 36, fortnite);
+        Partida partidaP2 = new Partida("2023_1_6", 2000, 40, fortnite);
+        Partida partidaP3 = new Partida("2023_12_2", 3600, 10, ajedrez);
+        Partida partidaP4 = new Partida("2023_6_16", 700, 80, tetris);
+        Partida partidaP5 = new Partida("2023_3_4", 1600, 22, fortnite);
+        Partida partidaP6 = new Partida("2023_2_24", 1000, 18, apexLegends);
+        Partida partidaP7 = new Partida("2023_1_7", 4600, 0, ajedrez);
+
+        jugador.agregarPartida(partidaJ1);
+        jugador.agregarPartida(partidaJ2);
+        jugador.agregarPartida(partidaJ3);
+        jugador.agregarPartida(partidaJ4);
+        jugador.agregarPartida(partidaJ5);
+        jugador.agregarPartida(partidaJ6);
+        jugador.agregarPartida(partidaJ7);
 
         compe.agregarJuego(tetris);
         compe.agregarJuego(fortnite);
